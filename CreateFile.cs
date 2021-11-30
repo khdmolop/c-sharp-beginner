a30e5da57eea9fa67b82242d61353fa5989b7ee5
khdmolop <83680629+khdmolop@users.noreply.github.com>
2021-11-30 04:22:23 +0100

    Add files via upload

---
+++ CreateFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+namespace createFile1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            writeFile(@"C:\Users\khali\Documents\sameh.txt", "hell file");
+        }
+        static void writeFile(string path,string data)
+        {
+            StreamWriter writeData = new StreamWriter(path);
+            writeData.WriteLine(data);
+            writeData.Close();
+
+        }
+    }
+}
