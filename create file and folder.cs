a414b8c3c60cf3b8cdf75751534256e0becdf0ed
khdmolop <83680629+khdmolop@users.noreply.github.com>
2021-11-30 22:21:47 +0100

    Add files via upload

---
+++ create file and folder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"C:\Users\khali\OneDrive\Desktop\khalilf\g\pop");
+            //لانشاء المجلداتcreate folder
+             StreamWriter sw = new StreamWriter(@"C:\Users\khali\OneDrive\Desktop\khalilf\g\pop\pop1.txt", true);
+            sw.WriteLine("kakbitamer");
+            sw.Close();
+            //create text
+        }
+    }
+}
